e30c0f278bccc159c526fc0b51b2e9f5f3666a98
JackStarkmk50 <rajendran01jack@gmail.com>
2026-04-19 12:11:28 +0530

    test

--- robo_dataset.py
+++ robo_dataset.py
@@ -2,28 +2,29 @@
 from roboflow import Roboflow
 from ultralytics import YOLO
 import torch
-rf = Roboflow(api_key="kWhhzcsndL8DHL6oYFfF")
-project = rf.workspace("final-year-project-yiiyi").project("indian-currency-coin-detection")
-version = project.version(5)
-dataset = version.download("yolo26")
-                
 import os
-os.environ["DATASET_LOCATION"] = dataset.location
-print(f"Dataset downloaded to: {dataset.location}")
+                
+if __name__ == '__main__':
+    rf = Roboflow(api_key="kWhhzcsndL8DHL6oYFfF")
+    project = rf.workspace("final-year-project-yiiyi").project("indian-currency-coin-detection")
+    version = project.version(5)
+    dataset = version.download("yolo26")
+    os.environ["DATASET_LOCATION"] = dataset.location
+    print(f"Dataset downloaded to: {dataset.location}")
 
 
-# 2. Load the YOLO model
-# 'yolo11n.pt' is the fastest/lightest; use 's', 'm', 'l', or 'x' for more accuracy
-model = YOLO("yolo26n.pt") 
+    # 2. Load the YOLO model
+    # 'yolo11n.pt' is the fastest/lightest; use 's', 'm', 'l', or 'x' for more accuracy
+    model = YOLO("yolo26n.pt") 
 
-# 3. Train with High GPU Utilization settings
-results = model.train(
-    data=f"{dataset.location}/data.yaml", # Path to the downloaded dataset
-    epochs=100,                           # Number of training loops
-    imgsz=640,                            # Image size
-    batch=-1,                             # AUTO-BATCH: Finds max size for your GPU VRAM
-    workers=8,                            # Parallel threads for data loading (adjust based on CPU)
-    device=0,                             # Use GPU '0'. Use [0, 1] for multi-GPU
-    amp=True,                             # Automatic Mixed Precision (faster training)
-    plots=True                            # Save progress charts
-)
+    # 3. Train with High GPU Utilization settings
+    results = model.train(
+        data=f"{dataset.location}/data.yaml", # Path to the downloaded dataset
+        epochs=100,                           # Number of training loops
+        imgsz=640,                            # Image size
+        batch=-1,                             # AUTO-BATCH: Finds max size for your GPU VRAM
+        workers=8,                            # Parallel threads for data loading (adjust based on CPU)
+        device=0,                             # Use GPU '0'. Use [0, 1] for multi-GPU
+        amp=True,                             # Automatic Mixed Precision (faster training)
+        plots=True                            # Save progress charts
+    )
